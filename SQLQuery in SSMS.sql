47a272f0cb1fe15e765908e3faa4d6f02b72d588
Mani Shankar Jha <153589964+manishankarjha@users.noreply.github.com>
2024-02-04 21:42:35 +0530

    Add files via upload

---
+++ SQLQuery in SSMS.sql
@@ -0,0 +1,12 @@
+With hotels as (
+select * from dbo.['2018$']
+union
+select * from dbo.['2019$']
+union
+select * from dbo.['2020$'] )
+
+Select * from hotels 
+left join dbo.market_segment$
+on hotels.market_segment = dbo.market_segment$.market_segment
+left join dbo.meal_cost$
+on hotels.meal = dbo.meal_cost$.meal
